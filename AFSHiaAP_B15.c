e96cb84751c6ef891b0206a27fcc110fe29dd7b5
wildangbudhi <wildangb2@gmail.com>
2019-04-26 21:10:21 +0700

    Bismillah

--- AFSHiaAP_B15.c
+++ AFSHiaAP_B15.c
@@ -1,4 +1,4 @@
-define FUSE_USE_VERSION 28
+#define FUSE_USE_VERSION 28
 #include <fuse.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,42 +10,72 @@ define FUSE_USE_VERSION 28
 #include <sys/time.h>
 #include <libgen.h>
 
-static const char *dirpath = "/home/hp/shift4";
-//static const char *dirpath2 ="/home/hp/shift4";
+#define resetString(x) memset(x, 0, sizeof(x));
 
-static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-                       off_t offset, struct fuse_file_info *fi)
+//dir tiap ganti laptop harus ganti
+static const char *dirpath = "/home/wildangbudhi/Documents/coba";
+// static const char *dirpath = "/home/hp/shift4";
+
+static const char set1[]="qE1~ YMUR2\"`hNIdPzi%^t@(Ao:=CQ,nx4S[7mHFye#aT6+v)DfKL$r?bkOGB>}!9_wV']jcp5JZ&Xl|\\8s;g<{3.u*W-0";
+static const char set2[]="zi%^t@(Ao:=CQ,nx4S[7mHFye#aT6+v)DfKL$r?bkOGB>}!9_wV']jcp5JZ&Xl|\\8s;g<{3.u*W-0qE1~ YMUR2\"`hNIdP";
+static unsigned char encryptTable[256], decryptTable[256];
+int i;
+
+void encrypt(char* name)
 {
-  char fpath[1000];
-        if(strcmp(path,"/") == 0)
-        {
-                path=dirpath;
-                sprintf(fpath,"%s",path);
-        }
-        else sprintf(fpath, "%s%s",dirpath,path);
-        int res = 0;
+    for(i=0; i<strlen(name); i++) name[i] = encryptTable[(int) name[i]];
+}
 
-        DIR *dp;
-        struct dirent *de;
+void decrypt(char* name)
+{
+    for(i=0; i<strlen(name); i++) name[i] = decryptTable[(int) name[i]];
+}
 
-        (void) offset;
-        (void) fi;
+void Caesar()
+{
+    for(i=0; i<256; i++) encryptTable[i] = decryptTable[i] = i;
 
-        dp = opendir(fpath);
-        if (dp == NULL)
-                return -errno;
+    for(i=0; i<strlen(set1); i++)
+        if(set2[i] != set1[i]) {
+            encryptTable[(int) set1[i]] = set2[i];
+            decryptTable[(int) set2[i]] = set1[i];
+        }  
+}
 
-        while ((de = readdir(dp)) != NULL) {
-                struct stat st;
-                memset(&st, 0, sizeof(st));
-                st.st_ino = de->d_ino;
-                st.st_mode = de->d_type << 12;
-                res = (filler(buf, de->d_name, &st, 0));
-                        if(res!=0) break;
-        }
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
+{
+    char fpath[1000], fname[1000];
+    int res = 0;
+    DIR *dp;
+    struct dirent *de;
+    (void) offset;
+    (void) fi;
 
-        closedir(dp);
-        return 0;
+    if(strcmp(path,"/") == 0)
+    {
+        path=dirpath;
+        sprintf(fpath,"%s",path);
+    }
+    else {
+        strcpy(fname, path);
+        encrypt(fname);
+        sprintf(fpath, "%s%s",dirpath,fname);
+    }
+
+    dp = opendir(fpath);
+    if (dp == NULL) return -errno;
+
+    while ((de = readdir(dp)) != NULL) {
+            struct stat st;
+            memset(&st, 0, sizeof(st));
+            st.st_ino = de->d_ino;
+            st.st_mode = de->d_type << 12;
+            res = (filler(buf, de->d_name, &st, 0));
+                    if(res!=0) break;
+    }
+
+    closedir(dp);
+    return 0;
 }
 
 
@@ -63,7 +93,7 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
         return 0;
 }
 
-tatic int xmp_mkdir(const char *path,mode_t mode)
+static int xmp_mkdir(const char *path,mode_t mode)
 {
     int res;
     char fpath[1000];
